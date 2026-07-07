b677de3c764bf7df6de311602298f0c8fd03df19
Rex-International <gabrielmichaelofficialtz@gmail.com>
2026-07-07 10:33:13 +0300

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Star-Universal-Trade-max/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
